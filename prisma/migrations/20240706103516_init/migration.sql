8201cee6b5479777fb586a1b37928576c08a389c
Ayokunle Adesanya <adesanyaayokunle@gmail.com>
2024-07-30 00:33:47 +0100

    update prisma

--- prisma/migrations/20240706103516_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "status" TEXT DEFAULT 'pending',
-    "tableNumber" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
